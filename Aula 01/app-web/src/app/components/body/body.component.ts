eb28f468da986297312db98051c33cfb1f027b83
Evandro dos Santos Dias <vando86206@gmail.com>
2025-07-04 10:36:36 -0300

    fechamento-aula-01

--- Aula 01/app-web/src/app/components/body/body.component.ts
+++ Aula 01/app-web/src/app/components/body/body.component.ts
@@ -11,12 +11,33 @@ import { Component } from '@angular/core';
 export class BodyComponent {
   title = "Testando"
 
-  cards:string[] = [
-    'card 1', 'card 2', 'card 5',
-    'card 4', 'card 5', 'card 6',
-    'card 7', 'card 8', 'card 9',
-  ]
+  // cards:string[] = [
+  //   'card 1', 'card 2', 'card 5',
+  //   'card 4', 'card 5', 'card 6',
+  //   'card 7', 'card 8', 'card 9',
+  // ]
 
-  
+  cards = [
+    {
+      nome: 'Tênis de Corrida',
+      // imagem: 'assets/img/tenis.jpg',
+      imagem: './img/tenis.png',
+      descricao: 'Confortável e resistente.'
+    },
+    {
+      nome: 'Chinelo de Praia',
+      // imagem: 'assets/img/chinelo.jpg',
+      imagem: './img/chinelo.png',
+      descricao: 'Ideal para dias quentes.'
+    },
+    {
+      nome: 'Bota de Couro',
+      // imagem: 'assets/img/bota.jpg',
+      imagem: './img/Bota de Couro.png',
+      descricao: 'Estilo e durabilidade.'
+    }
+  ];
+
+
 
 }
