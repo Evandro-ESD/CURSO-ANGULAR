87022b8edc28dd954fbd0fdf3d3d8c1827c88f28
Evandro dos Santos Dias <vando86206@gmail.com>
2025-07-08 14:20:02 -0300

    services-com-get

--- Aula 03/gerenciamento-estado/src/app/app.routes.ts
+++ Aula 03/gerenciamento-estado/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { Contador04Component } from './components/contador04/contador04.component';
 
 export const routes: Routes = [
+    {path: 'contador', component: Contador04Component}
 ];
