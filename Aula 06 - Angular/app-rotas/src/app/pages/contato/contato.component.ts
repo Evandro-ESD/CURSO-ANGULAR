e86e1d02e9ab2fcc6857fe810665dca162383b39
Evandro dos Santos Dias <vando86206@gmail.com>
2025-07-16 11:18:55 -0300

    contato-comentários

--- Aula 06 - Angular/app-rotas/src/app/pages/contato/contato.component.ts
+++ Aula 06 - Angular/app-rotas/src/app/pages/contato/contato.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-contato',
   imports: [ReactiveFormsModule,],
@@ -9,25 +8,25 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
-  formContato: FormGroup
+  formContato: FormGroup // para guardar dados do form
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) { // melhorar o FormGrop
     this.formContato = this.fb.group({
-      nome: ['', [Validators.required]],
+      nome: ['', [Validators.required]], // Validadores dos campos
       email: ['', [Validators.required, Validators.email]],
       mensagem: ['', [Validators.required, Validators.minLength(10)]]
     })
   }
 
-  enviarForm() {
-    if(this.formContato.valid){
-      console.log("Formulário: ", this.formContato.value)
-      this.formContato.reset()
-    }else 
-      {
-        alert(`Preencha todos os campos`)
-        console.log("Erro: ....")}
-        
+  enviarForm() { // função para submeter formulário
+    if (this.formContato.valid) {
+      // console.log("Formulário: ", this.formContato.value) // debug para verificar dados do formuário
+      this.formContato.reset() // limpa os campos do formulário
+      confirm("Confirma o envio dos dados") // mensagem para confirmar o envio dos dados
+    } else {
+      alert(`Preencha todos os campos`) // mensagem para avisar erro em algum preenchimento
+      // console.log("Erro: ....")
+    }
   }
 
 }
