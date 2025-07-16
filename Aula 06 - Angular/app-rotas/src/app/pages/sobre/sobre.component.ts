ab4a8d508ecae5e2912fe7e310c0c11ae81b72dc
Evandro dos Santos Dias <vando86206@gmail.com>
2025-07-16 13:17:12 -0300

    add-imgae

--- Aula 06 - Angular/app-rotas/src/app/pages/sobre/sobre.component.ts
+++ Aula 06 - Angular/app-rotas/src/app/pages/sobre/sobre.component.ts
@@ -6,24 +6,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.css'
 })
-export class SobreComponent implements OnInit{
+export class SobreComponent implements OnInit {
 
-  slideAtual:number = 0
+  slideAtual: number = 0
 
   slides = [
-    {src: "/instiucional/campeonato.png" , alt: "", descricao: "Divulgação do próximo torneio oficial promovido pela Arena Chess Store."},
-    {src: "/instiucional/fundo.png" , alt: "", descricao: "Foto da loja Arena Chess Store, especializada em artigos de xadrez."},
-    {src: "/instiucional/julia.png" , alt: "", descricao: "A Grande Mestre dará uma palestra exclusiva na loja."},
+    { src: "/instiucional/campeonato.png", alt: "", descricao: "Divulgação do próximo torneio oficial promovido pela Arena Chess Store." },
+    { src: "/instiucional/fundo.png", alt: "", descricao: "Foto da loja Arena Chess Store, especializada em artigos de xadrez." },
+    { src: "/instiucional/julia.png", alt: "", descricao: "A Grande Mestre dará uma palestra exclusiva na loja." },
   ]
 
   ngOnInit(): void {
-    setInterval
+    setInterval(() => {
+      this.proxima()
+    }, 3000)
   }
 
-  proxima(){
+  proxima() {
     this.slideAtual = (this.slideAtual + 1) % this.slides.length
   }
-  anterior(){
+  anterior() {
     this.slideAtual = (this.slideAtual - 1 + this.slides.length) % this.slides.length
   }
 
